8247bb12876785701ea9c6ba90f5139725cca651
Paul Bunn <paulbunn2004@gmail.com>
2023-12-02 19:03:57 +1100

    day 2

---
+++ aoc-02-02/Program.cs
@@ -0,0 +1,32 @@
+﻿int sum = 0;
+
+File.ReadLines("input.txt").ToList().ForEach(line => {
+    var gameSet = line.Split(":")[1].Trim();
+    var gameRounds = gameSet.Split(";").Select(r => r.Trim()).ToList();
+    
+    Dictionary<string, int> colors = new Dictionary<string, int> {
+        { "red", 1 },
+        { "green", 1 },
+        { "blue", 1 }
+    };
+
+    gameRounds.ForEach(r => {        
+        r.Split(",").ToList().ForEach(c => {
+            
+            var color = c.Trim().Split(" ")[1];
+            var count = int.Parse(c.Trim().Split(" ")[0]);
+            
+            colors[color] = Math.Max(count, colors[color]);
+        });
+    });
+
+    int power = colors["red"] * colors["green"] * colors["blue"];
+    sum += power;
+
+});
+
+Console.WriteLine($"The sum of game powers is {sum}");
+
+
+
+
---
+++ aoc-02/Program.cs
@@ -0,0 +1,37 @@
+﻿Dictionary<string, int> colors = new Dictionary<string, int> {
+    { "red", 12 },
+    { "green", 13 },
+    { "blue", 14 }
+};
+
+Stack<int> possibleGameStack = new Stack<int>();
+
+File.ReadLines("input.txt").ToList().ForEach(line => {
+    var gameKey = int.Parse(line.Split(":")[0].Trim().Split(" ")[1]);
+    possibleGameStack.Push(gameKey);
+
+    var gameSet = line.Split(":")[1].Trim();
+    var gameRounds = gameSet.Split(";").Select(r => r.Trim()).ToList();
+
+    bool gameFailed = false;
+
+    gameRounds.ForEach(r => {
+        r.Split(",").ToList().ForEach(c => {
+            var color = c.Trim().Split(" ")[1];
+            var count = int.Parse(c.Trim().Split(" ")[0]);
+
+            if (count > colors[color]) {             
+                gameFailed = true;
+            };           
+        });
+    });
+
+    if (gameFailed) {
+        possibleGameStack.Pop();
+    }    
+});
+
+int sum = 0;
+possibleGameStack.ToList().ForEach(g => sum += g);
+
+Console.WriteLine($"The sum of possible game ids is {sum}");
