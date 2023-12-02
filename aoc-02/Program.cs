fa84d0cd0cd3358d33471e7e3b336547215aac8a
Paul Bunn <paulbunn2004@gmail.com>
2023-12-02 19:06:16 +1100

    .

--- aoc-02/Program.cs
+++ aoc-02/Program.cs
@@ -8,27 +8,25 @@ Stack<int> possibleGameStack = new Stack<int>();
 
 File.ReadLines("input.txt").ToList().ForEach(line => {
     var gameKey = int.Parse(line.Split(":")[0].Trim().Split(" ")[1]);
-    possibleGameStack.Push(gameKey);
-
     var gameSet = line.Split(":")[1].Trim();
     var gameRounds = gameSet.Split(";").Select(r => r.Trim()).ToList();
 
     bool gameFailed = false;
+
+    possibleGameStack.Push(gameKey);
 
     gameRounds.ForEach(r => {
         r.Split(",").ToList().ForEach(c => {
             var color = c.Trim().Split(" ")[1];
             var count = int.Parse(c.Trim().Split(" ")[0]);
 
-            if (count > colors[color]) {             
-                gameFailed = true;
-            };           
+            if (count > colors[color])
+                gameFailed = true;            
         });
     });
 
-    if (gameFailed) {
+    if (gameFailed)
         possibleGameStack.Pop();
-    }    
 });
 
 int sum = 0;
